7976a5904fb4848fb31f6dc377b1b2078af6a46e
Gal Bashan <gal@epsagon.com>
2019-12-15 22:24:33 +0200

    [master] Adding reporting

--- path_finder/point.py
+++ path_finder/point.py
@@ -1,7 +1,9 @@
 import math
 
 from path_finder.grid import Point
+from functools import lru_cache
 
 
+@lru_cache()
 def distance(p1: Point, p2: Point) -> int:
     return int(math.fabs(p1[0] - p2[0]) + math.fabs(p1[1] - p2[1]))
